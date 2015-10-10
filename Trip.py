522b71da21f4650ef6202d7a6b607505237f86d0
JamLew404 <james@lewthwaite.com.au>
2015-10-10 20:21:18 +1000

    Github Test

--- Trip.py
+++ Trip.py
@@ -1,2 +1,26 @@
 __author__ = 'James'
 
+
+class Error(Exception):
+    def __init__(self, error):
+        super(Error, self).__init__(Exception)
+
+
+class Country:
+    def __init__(self, name, currency_code, currency_symbol):
+        self.name = name
+        self.currency_code = currency_code
+        self.currency_symbol = currency_symbol
+        amount = int(input("Enter your amount: "))
+        amount = float(amount)
+        print({}, {}, {} + format(amount, ".2f"))
+        if name.isalpha() is False:
+            print("Country can only have letters")
+
+
+    def __str__(self, **kwargs):
+        return "{}, {}, {}".format(self.name, self.currency_code, self.currency_symbol)
+
+
+# class Details:
+#     def
