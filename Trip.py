150ab7e6869c70e5a2e25f9164ca858bfe7c05e0
JamLew404 <james@lewthwaite.com.au>
2015-10-10 21:10:33 +1000

    Github Test

--- Trip.py
+++ Trip.py
@@ -3,24 +3,31 @@ __author__ = 'James'
 
 class Error(Exception):
     def __init__(self, error):
-        super(Error, self).__init__(Exception)
+        super().__init__(error)
 
 
 class Country:
+    # This is used to define the values
     def __init__(self, name, currency_code, currency_symbol):
         self.name = name
         self.currency_code = currency_code
         self.currency_symbol = currency_symbol
-        amount = int(input("Enter your amount: "))
-        amount = float(amount)
-        print({}, {}, {} + format(amount, ".2f"))
         if name.isalpha() is False:
-            print("Country can only have letters")
+                print("Country can only have letters")
 
+        # amount = int(input("Enter your amount: "))
+        # amount = float(amount)
+        # print({}, {}, {} + format(amount, ".2f"))
 
     def __str__(self, **kwargs):
-        return "{}, {}, {}".format(self.name, self.currency_code, self.currency_symbol)
+        return " {} {} {} ".format(self.name, self.currency_code, self.currency_symbol)
+
+    def amount_format(self, amount):
+        if amount < 0:
+            raise Error("The amount cannot be less than 0")
+        amount = float(amount)
 
 
-# class Details:
-#     def
+class Details:
+    def __int__(self, locations):
+        self.locations = locations
